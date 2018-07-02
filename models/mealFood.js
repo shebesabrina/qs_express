cb4c8112c20c6f0628d0e45629d0b434577778de
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-02 17:36:52 -0600

    more WIP on dynamic message

--- models/mealFood.js
+++ models/mealFood.js
@@ -6,24 +6,18 @@ pry = require('pryjs')
 class MealFood {
   static create(attributes){
     return database('meal_foods')
-    .insert(attributes)
+    .insert({meal_id: attributes.meal_id, food_id: attributes.id})
     .returning(this.message(attributes))
   }
 
   static message(attributes) {
-    let food_id = attributes.id
-    let meal_id = attributes.meal_id
-    return database.raw(
-      "SELECT foods.name AS food_name, meals.name AS meal_name FROM meal_foods INNER JOIN meals on meals.id = meal_foods.meal_id INNER JOIN foods ON foods.id = meal_foods.food_id WHERE meal_foods.meal_id = ? AND meal_foods.food_id = ?;",
-      [meal_id, food_id]
-    )
-    .then((data) => {
-      eval(pry.it)
-      f_name = data.rows[0].food_name
-      m_name = data.rows[0].meal_name
-      return {food_name: f_name, meal_name: m_name}
-    })
-  };
+    return database('meal_foods')
+    .select({mealName: 'meals.name'}, {foodName: 'foods.name'})
+    .where({meal_id: attributes.meal_id, food_id: attributes.id})
+    .join('meals', {'meals.id': 'meal_foods.meal_id'})
+    .join('foods', {'foods.id': 'meal_foods.food_id'})
+    .first()
+  }
 }
 
 module.exports = MealFood
