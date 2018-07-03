2d04933e63930f88ddc7df910011c0198cc05b47
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-03 13:07:24 -0600

    adding procfile

--- db/seeds/production/seeds.js
+++ db/seeds/production/seeds.js
@@ -1,5 +1,4 @@
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
   return knex.raw('TRUNCATE meals RESTART IDENTITY CASCADE')
     .then(function() {
       return Promise.all([
