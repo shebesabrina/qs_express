a4f073448b541fd4825ea0bc74cfbda47826c73a
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-16 13:36:16 -0600

    playing around with fetch

--- models/recipe.js
+++ models/recipe.js
@@ -4,7 +4,11 @@ const database = require('knex')(configuration)
 
 class Recipe {
   static all(food_id) {
-    
+    const allRecipes = () => {
+      fetch('http://api.yummly.com/v1/api/recipes?_app_id=2e2c0a2e&_app_key=083edf3221f3c68df434cef4487c8df9y&q=banana')
+      .then((response) => console.log(response))
+      .catch((error) => console.error({ error }));
+    }
   }
 
 }
