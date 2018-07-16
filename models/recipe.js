6f9df408c740409563ac2728ed728c669d30f627
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-16 13:44:17 -0600

    adding isomorphic fetch package

--- models/recipe.js
+++ models/recipe.js
@@ -1,6 +1,11 @@
+require('isomorphic-fetch');
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
+
+
+fetch('http://api.yummly.com/v1/api/recipes?_app_id=2e2c0a2e&_app_key=083edf3221f3c68df434cef4487c8df9y&q=banana')
+.then((response) => console.log(response))
 
 class Recipe {
   static all(food_id) {
