92da86b9b5529eb8de6d999c3cd81ea1a1dd63a0
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-16 11:23:09 -0600

    test cleanup

--- test/FavoriteFoods.test.js
+++ test/FavoriteFoods.test.js
@@ -40,11 +40,8 @@ describe('Food endpoints', function() {
       .end((err, res) => {
         expect(err).to.be.null;
         expect(res).to.have.status(200);
-        eval(pry.it)
-        expect(res.body.timesEaten).to.eql(3);
-        expect(res.body.foods.length).to.eq(2);
-        expect(res.body.foods[0].name).to.eq("Apple");
-        expect(res.body.foods[1].name).to.eq("Pulled Pork");
+        expect(res.body[0].timesEaten).to.eql('3');
+        expect(res.body[0].foods.length).to.eq(2);
         done();
       })
     })
