1d2434b469367caeb797c8a5d6bac6f4faf9837c
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-16 09:12:43 -0600

    building test for favorite foods endpoint

---
+++ test/FavoriteFoods.test.js
@@ -0,0 +1,54 @@
+process.env.NODE_ENV = 'test'
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const assert = chai.assert; // import { assert } from "chai" //
+const expect = chai.expect; // import { expect } from "chai" //
+const pry = require('pryjs')
+const app = require('../app')
+
+chai.use(chaiHttp);
+//
+const environment = 'test'
+const configuration = require('../knexfile')[environment]
+const knex = require('knex')(configuration)
+
+
+/* Clean database and run migrations/seeds before each test*/
+describe('Food endpoints', function() {
+  beforeEach((done) => {
+    knex.migrate.latest()
+    .then(() => {
+      knex.seed.run()
+      .then(() => {
+        done();
+      })
+    });
+  });
+
+  afterEach((done) => {
+    knex.migrate.rollback()
+    .then(() => {
+      done();
+    });
+  });
+
+  describe("GET /api/v1/favorite_foods", () => {
+    it('returns foods added to more than one meal', (done) => {
+      chai.request(app)
+      .get('/api/v1/favorite_foods')
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body.timesEaten).to.eql(2);
+        expect(res.body.foods.length).to.eq(2);
+        expect(res.body.foods[0].name).to.eq("Banana");
+        expect(res.body.foods[1].name).to.eq("Meatloaf");
+        done();
+      })
+    })
+  })
+
+
+
+});
