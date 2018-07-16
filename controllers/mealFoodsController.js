9fdb879fa7bc2feaddbbf420daad50a72ff97c99
Margaret Williford <margaretwilliford4@gmail.com>
2018-07-16 09:05:22 -0600

    fixing indentation

--- controllers/mealFoodsController.js
+++ controllers/mealFoodsController.js
@@ -6,15 +6,15 @@ class MealFoodsController {
   static create(request, response, next){
     if(Meal.find(request.params.meal_id) && Food.find(request.params.food_id)) {
       MealFood.create(request.params)
-      .then(mealFood => {
-        if(mealFood){
-          let result = mealFood.rows[0]
-          let message = `Successfully added ${result.food_name} to ${result.meal_name}`
-          response.status(201).json({message: message})
-        } else {
-          response.sendStatus(404)
-        }
-      })
+        .then(mealFood => {
+          if(mealFood){
+            let result = mealFood.rows[0]
+            let message = `Successfully added ${result.food_name} to ${result.meal_name}`
+            response.status(201).json({message: message})
+          } else {
+            response.sendStatus(404)
+          }
+        })
     } else {
       response.sendStatus(404)
     }
@@ -22,15 +22,15 @@ class MealFoodsController {
 
   static destroy(request, response, next) {
     MealFood.destroy(request.params)
-    .then((mealFood) => {
-      if(mealFood) {
-        let result = mealFood.rows[0]
-        let message = `Successfully removed ${result.food_name} from ${result.meal_name}`
-        response.json({message: message})
-      } else {
-        response.sendStatus(404)
-      }
-    })
+      .then((mealFood) => {
+        if(mealFood) {
+          let result = mealFood.rows[0]
+          let message = `Successfully removed ${result.food_name} from ${result.meal_name}`
+          response.json({message: message})
+        } else {
+          response.sendStatus(404)
+        }
+      })
   }
 }
 
